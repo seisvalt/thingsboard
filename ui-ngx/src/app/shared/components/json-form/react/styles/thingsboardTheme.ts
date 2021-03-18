91914eb123330fbcb3616b8832a06fbce7340567
root <jose.vargas@globalcircuit.co>
2021-03-18 15:51:30 -0500

    new template for gc

--- ui-ngx/src/app/shared/components/json-form/react/styles/thingsboardTheme.ts
+++ ui-ngx/src/app/shared/components/json-form/react/styles/thingsboardTheme.ts
@@ -35,7 +35,8 @@ import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 import { mergeDeep } from '@core/utils';
 
-const PRIMARY_COLOR = '#305680';
+/*const PRIMARY_COLOR = '#305680';*/
+const PRIMARY_COLOR = '#001c46';
 const SECONDARY_COLOR = '#527dad';
 const HUE3_COLOR = '#a7c1de';
 
